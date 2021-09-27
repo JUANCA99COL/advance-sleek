a69afe9e9c5f059f62e6389a35e7bd46dc315279
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2021-09-27 22:35:23 +1300

    no php

--- form-to-email.php
+++ form-to-email.php
@@ -1,18 +1,18 @@
 <?php
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
+  // $name = $_POST['name'];
+  // $visitor_email = $_POST['email'];
+  // $message = $_POST['message'];
 ?>
 
 <!-- compose email message  -->
 
 <?php
-	$email_from = '40dd3d45641d3ba44879cd0a6337289a';
+	// $email_from = '40dd3d45641d3ba44879cd0a6337289a';
 
-	$email_subject = "New Form submission";
+	// $email_subject = "New Form submission";
 
-	$email_body = "You have received a new message from the user $name.\n".
-                            "Here is the message:\n $message".
+	// $email_body = "You have received a new message from the user $name.\n".
+  //                           "Here is the message:\n $message".
 
   
 
@@ -20,14 +20,14 @@
 
 
 
-  $to = "40dd3d45641d3ba44879cd0a6337289a";
+  // $to = "40dd3d45641d3ba44879cd0a6337289a";
 
 
-  $headers = "From: $email_from \r\n";
+  // $headers = "From: $email_from \r\n";
 
-  $headers .= "Reply-To: $visitor_email \r\n";
+  // $headers .= "Reply-To: $visitor_email \r\n";
 
-  mail($to,$email_subject,$email_body,$headers);
+  // mail($to,$email_subject,$email_body,$headers);
 
 
 
@@ -35,39 +35,39 @@
 // <!-- securing from global injection  -->
 
 
-function IsInjected($str)
-{
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
+// function IsInjected($str)
+// {
+//     $injections = array('(\n+)',
+//            '(\r+)',
+//            '(\t+)',
+//            '(%0A+)',
+//            '(%0D+)',
+//            '(%08+)',
+//            '(%09+)'
+//            );
                
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
+//     $inject = join('|', $injections);
+//     $inject = "/$inject/i";
     
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-}
+//     if(preg_match($inject,$str))
+//     {
+//       return true;
+//     }
+//     else
+//     {
+//       return false;
+//     }
+// }
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-?>
+// if(IsInjected($visitor_email))
+// {
+//     echo "Bad email value!";
+//     exit;
+// }
+// ?>
 
-<?php
+// <?php
 /* Mailhandler for the main email address */
-header ("Location: mailto:40dd3d45641d3ba44879cd0a6337289a");
+// header ("Location: mailto:40dd3d45641d3ba44879cd0a6337289a");
 
-exit();
+// exit();
